a44104f1ad5677ae24ef2befbad57f38be0a905e
AlexTheAmateur <awu4921@gmail.com>
2021-11-13 16:23:20 -0600

    adding all conditional statements for windows confirm

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,39 @@
 // Assignment code here
 
 var generatePassword = function() {
+  // Our criteria
+  var passwordCharacters = '';
+
   var promptLength = window.prompt("How long would you like your password?");
+  if (promptLength != parseInt(promptLength)) {
+    window.alert("Non-numeric response. Please enter a valid number such as '420'");
+    generatePassword();
+  }
+  else {
+    password.length = parseInt(promptLength);
+  }
+
   var promptLowercase = window.confirm("Would you like lowercase letters in your password?");
   var promptUppercase = window.confirm("Would you like uppercase letters in your password?");
   var promptNumbers = window.confirm("Would you like numbers in your password?");
   var promptSpecial = window.confirm("Would you like special characters in your password?");
+  if (promptLowercase) {
+    passwordCharacters = passwordCharacters + 'ascdefghijklmnopqrstuvwxyz'
+  }
+  if (promptUppercase) {
+    passwordCharacters = passwordCharacters + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  }
+  if (promptNumbers) {
+    passwordCharacters = passwordCharacters + '0123456789'
+  }
+  if (promptSpecial) {
+    passwordCharacters = passwordCharacters + '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+  }
+
+  // for (let i = 0; i < password.length; i++) {
+  //   password = password + passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length));
+  // }
+  // return;
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
